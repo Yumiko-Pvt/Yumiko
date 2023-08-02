dc694c8fd1a58b1a2d1dd195374da04811c1a14f
Yumiko-Pvt <141236136+Yumiko-Pvt@users.noreply.github.com>
2023-08-02 17:15:42 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+import re
+import setuptools
+
+with open("requirements.txt", encoding="utf-8") as r:
+    requires = [i.strip() for i in r]
+
+with open("Yumikolab/__init__.py", encoding="utf-8") as f:
+    version = re.findall(r"__version__ = \"(.+)\"", f.read())[0]
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+  
+setuptools.setup(
+    name="Yumikolab",
+    version=version,
+    author="Yumiko-Pvt",
+    long_description=readme,
+    license='GNU General Public License v3.0',
+    description='Python package for telegram bots',                           
+    package_data={
+      "Yumikolab": ["py.typed"],
+    },
+    url="https://github.com/Yumiko-Pvt/Yumiko",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet",
+        "Topic :: Communications",
+        "Topic :: Communications :: Chat",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ],
+    python_requires=">=3.6",
+    install_requires=requires,
+    py_modules=["Yumikolab"],
+)

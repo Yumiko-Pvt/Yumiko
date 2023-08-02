c59977b44a44251035aa450cdd9de0c06b301248
Yumiko-Pvt <141236136+Yumiko-Pvt@users.noreply.github.com>
2023-08-02 17:22:44 +0530

    Create __init__.py

---
+++ Yumikolab/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.1.0'
+__author__ = 'YumikoLab'
+__license__ = 'GNU Lesser General Public License v3.0 (LGPL-3.0)'
+__description__ = 'A Python library for Telegram, similar to Pyrogram.'
+
+def Yumikolab():
+  print(
+    "Yumikolab Successfully Installed.." + "\n"
+    f"Version : {__version__}"\n
+    f"License : {__license__}"\n
+    f"Author : {__author__}"\n
+  )
